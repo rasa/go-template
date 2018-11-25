86a5180ee6831111af000b4b5f7ec8b57ee629a6
Ross Smith II <ross@smithii.com>
2018-11-25 12:27:59 -0800

    Remove .exe, add CPUs to main

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
   "os"
   "path/filepath"
   "runtime"
+	"strings"
 
   "github.com/rasa/go-template/version"
 )
 
 func main() {
-	fmt.Printf("%s: Version %s (%s)\n", filepath.Base(os.Args[0]), version.VERSION, version.GITCOMMIT)
-	fmt.Printf("Built with go %s for %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	basename := filepath.Base(os.Args[0])
+	progname := strings.TrimSuffix(basename, filepath.Ext(basename))
+
+	fmt.Printf("%s: Version %s (%s)\n", progname, version.VERSION, version.GITCOMMIT)
+	fmt.Printf("Built with %s for %s/%s (%d CPUs/%d GOMAXPROCS)\n",
+		runtime.Version(),
+		runtime.GOOS,
+		runtime.GOARCH,
+		runtime.NumCPU(),
+		runtime.GOMAXPROCS(-1))
 }
