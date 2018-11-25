634902eca7fc438b26382980a9dfe94f52844c85
Ross Smith II <ross@smithii.com>
2018-11-25 10:22:36 -0800

    Add go version and GOOS/GOARCH to main.go

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
   "fmt"
   "os"
   "path/filepath"
-  
+  "runtime"
+
   "github.com/rasa/go-template/version"
 )
 
 func main() {
 	fmt.Printf("%s: Version %s (%s)\n", filepath.Base(os.Args[0]), version.VERSION, version.GITCOMMIT)
+	fmt.Printf("Built with go %s for %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
